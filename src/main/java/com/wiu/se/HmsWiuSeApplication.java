f383500d6697cd0e0db042f953b5857ecd8037c0
vinay <vinay@LAPTOP-GH7PUIOE>
2022-02-23 11:19:34 -0600

    Initial Commit...

---
+++ src/main/java/com/wiu/se/HmsWiuSeApplication.java
@@ -0,0 +1,13 @@
+package com.wiu.se;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsWiuSeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsWiuSeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wiu/se/HmsWiuSeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiu.se;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsWiuSeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
